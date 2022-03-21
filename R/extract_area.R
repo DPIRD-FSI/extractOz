f94cf61384f7b36d81f08074ac988e56425e0c8d
Adam Sparks <adamhsparks@gmail.com>
2022-03-21 16:35:33 +0800

    Add generic extract_area() for user-supplied sf objects

---
+++ R/extract_area.R
@@ -0,0 +1,44 @@
+
+#' Extract an Area of Interest Using Australian GPS Coordinates
+#'
+#' A generic function to extract an area of interest for user-provided
+#' \acronym{GPS} coordinates.
+#'
+#' @param x a `data.frame` of \acronym{GPS} coordinates in two columns.
+#' @param coords names or numbers of the numeric columns holding coordinates in
+#'  `lonlat`.
+#' @param spatial a user-supplied [sf] object that contains information to
+#'  derive location information from.
+#' @param area the field in `spatial` that should be returned.
+#'
+#' @return a `data.frame` with the provided \acronym{GPS} coordinates and the
+#'  respective `area` value from `spatial`.
+#'
+#' @family extract functions
+#'
+#' @examples
+#' spatial <- aez
+#' locs <- data.frame(
+#'   site = c("Merredin", "Corrigin", "Tamworth"),
+#'   "x" = c(118.28, 117.87, 150.84),
+#'   "y" = c(-31.48, -32.33, -31.07)
+#' )
+#' extract_area(x = locs, coords = c("x", "y"), spatial = spatial, area = "AEZ")
+#' @export
+
+extract_area <- function(x, coords, spatial, area) {
+  if (missing(x)) {
+    stop(call. = FALSE,
+         "You must provide a `data.frame` of GPS coordinates.")
+  }
+
+  points_sf <- sf::st_as_sf(x = x,
+                            coords = coords,
+                            crs = sf::st_crs(spatial))
+
+  intersection <- as.integer(sf::st_intersects(points_sf, spatial))
+  area <- ifelse(is.na(intersection), "",
+                 as.character(spatial[[area]][intersection]))
+
+  return(cbind(x, area))
+}
