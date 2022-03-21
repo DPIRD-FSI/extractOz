d20d734de64fe697132d6fd67a6335eb99cfacb3
Adam Sparks <adamhsparks@gmail.com>
2022-03-21 11:10:28 +0800

    Add extract_soil_order()

---
+++ R/extract_soil_order.R
@@ -0,0 +1,37 @@
+
+#' Extract Soil Order from Australian GPS Coordinates
+#'
+#' @param x a `data.frame` of \acronym{GPS} coordinates in two columns.
+#' @param coords names or numbers of the numeric columns holding coordinates in
+#'  `lonlat`.
+#'
+#' @return a `data.frame` with the provided \acronym{GPS} coordinates and the
+#'  respective Digital Atlas of Australian Soils (\acronym{DAAS} soil order).
+#'
+#' @family extract functions
+#'
+#' @examples
+#' locs <- data.frame(
+#'   site = c("Merredin", "Corrigin", "Tamworth"),
+#'   "x" = c(118.28, 117.87, 150.84),
+#'   "y" = c(-31.48, -32.33, -31.07)
+#' )
+#' extract_soil_order(x = locs, coords = c("x", "y"))
+#' @export
+
+extract_soil_order <- function(x, coords) {
+  if (missing(x)) {
+    stop(call. = FALSE,
+         "You must provide a `data.frame` of GPS coordinates.")
+  }
+
+  points_sf <- sf::st_as_sf(x = x,
+                            coords = coords,
+                            crs = sf::st_crs(daas))
+
+  intersection <- as.integer(sf::st_intersects(points_sf, daas))
+  soil <- ifelse(is.na(intersection), "",
+                 as.character(daas$SOIL[intersection]))
+
+  return(cbind(x, soil))
+}
