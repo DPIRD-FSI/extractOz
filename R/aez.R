65f0e9115bb303b871c8923b204ffe5364e9800d
Adam H. Sparks <adamhsparks@gmail.com>
2022-12-12 14:23:58 +0800

    Use GPKG rather than SF object in R session

--- R/aez.R
+++ R/aez.R
@@ -1,7 +1,7 @@
 #' GRDC Australian Grains Agroecological Zones
 #'
 #' An [sf::sf()] object for displaying GRDC Australian Grains Agroecological
-#'  Zones
+#'  Zones.
 #'
 #' @section Coordinate Reference System:
 #' Geodetic CRS:  WGS 84
@@ -9,6 +9,13 @@
 #' @format An [sf::sf()] polygon object
 #'
 #' @source The Australian Grains Research and Development Corporation,
-#'  \acronym{GRDC}, \url{https://grdc.com.au/} under CC0 licence.
+#'  \acronym{GRDC}, \url{https://grdc.com.au/}, under CC0 licence.
 #'
-"aez"
+delayedAssign("aez", local({
+  try(
+    sf::read_sf(
+      system.file("extdata/aez.gpkg", package = "extractOz", mustWork = TRUE)
+    ),
+    silent = TRUE
+  )
+}))
