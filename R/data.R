5157f54000a014b1cfba655c53cc3ab7fa250ee0
Adam H. Sparks <adamhsparks@gmail.com>
2023-01-01 15:54:43 +0800

    Correct documentation for `aez` data object

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' GRDC Australian Grains Agroecological Zones
+#'
+#' @format An [sf::sf()] polygon object of the \acronym{GRDC} Australian Grains
+#'  Agroecological Zones with 18 rows and two variables.
+#' \describe{
+#'   \item{AEZ}{The agroecological zone name.}
+#'   \item{geometry}{The centroid of the agroecological zone.}
+#' }
+#'
+#' @section Coordinate Reference System:
+#'  Geodetic CRS:  WGS 84
+#'
+#' @source The Australian Grains Research and Development Corporation,
+#'  \acronym{GRDC}, \url{https://grdc.com.au/}, under CC0 licence.
+#'
+delayedAssign("aez", local({
+  try(
+    sf::read_sf(
+      system.file("extdata/aez.gpkg", package = "extractOz")
+    ),
+    silent = TRUE
+  )
+}))
