3be4cc5659ef846538d668d36290c7be2ba88859
Adam H. Sparks <adamhsparks@icloud.com>
2023-04-07 20:33:59 +0800

    Simplify .onLoad()

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 .onLoad <- function(libname, pkgname) {
-  datafile = sf::st_zm(sf::read_sf(system.file("extdata", "aez.gpkg",
-                                               package = "extractOz")))
-  assign("aez", datafile, envir = topenv())
+  assign("aez", sf::st_zm(sf::read_sf(
+    system.file("extdata", "aez.gpkg",
+                package = "extractOz")
+  )), envir = topenv())
 }
