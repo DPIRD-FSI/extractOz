bb227e1f44f03edfd6c023cb87b539447a6b9590
Adam H. Sparks <adamhsparks@icloud.com>
2023-04-08 19:03:31 +0800

    Delete zzz.R

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-  assign("aez", sf::st_zm(sf::read_sf(
-    system.file("extdata", "aez.gpkg",
-                package = "extractOz")
-  )), envir = topenv())
-}
