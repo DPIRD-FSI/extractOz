6d8b234383bb8378094ad7193dc4e60b9cbdadcc
Adam H. Sparks <adamhsparks@icloud.com>
2023-04-09 10:59:51 +0800

    Pass R build checks

--- R/extract_soil_order.R
+++ R/extract_soil_order.R
@@ -136,17 +136,3 @@ remove_cache = function() {
     file.remove(path)
   return(invisible(NULL))
 }
-
-#' @noRd
-# Check if user enables caching. If so use cache directory, else use tempdir()
-.set_cache <- function(cache) {
-  if (isTRUE(cache)) {
-    if (!dir.exists(manage_cache$cache_path_get())) {
-      manage_cache$mkdir()
-    }
-    cache_dir <- manage_cache$cache_path_get()
-  } else {
-    cache_dir <- tempdir()
-  }
-  return(cache_dir)
-}
