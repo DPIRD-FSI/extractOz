d14e18a25fbd4652ad9a7da9909acb8a9ed102a4
Adam H. Sparks <adamhsparks@icloud.com>
2023-04-09 17:07:03 +0800

    Simplify test code

--- tests/testthat/test-get_cache_file.R
+++ tests/testthat/test-get_cache_file.R
@@ -3,9 +3,8 @@ test_that("Test cache", {
   skip_on_cran()
   ## Test remove cache
   if (as.numeric(R.version$major) > 3) {
-    cache_file <- .get_cache_file()
 
-    expect_false(file.exists(cache_file))
+    expect_false(file.exists(.get_cache_file()))
 
     ## Cache the "cache", otherwise tests will become annoyingly slow
     file.copy(cache_file, to = paste0(cache_file, "-testthat"))
