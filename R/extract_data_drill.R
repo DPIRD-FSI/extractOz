2ef5113f15a036139d27968ae9d2e792bf9f7afb
Adam H. Sparks <adamhsparks@icloud.com>
2023-07-30 12:38:37 +0800

    Update documentation

--- R/extract_data_drill.R
+++ R/extract_data_drill.R
@@ -128,7 +128,7 @@
 #' # and WA
 #' locs <- list(
 #'     "Merredin" = c(x = 118.28, y = -31.48),
-#'     "Corrigin" = c(x = 117.87, -32.33),
+#'     "Corrigin" = c(x = 117.87, y = -32.33),
 #'     "Tamworth" = c(x = 150.84, y = -31.07)
 #' )
 #'
